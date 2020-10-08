7b72941fd61159cc892d73448086dc95767fa7df
Joaquín Cruz <joacocruz6@gmail.com>
2020-10-08 15:42:33 -0300

    feat(A_candies): finish problem and accepted

--- tarea_2/A_candies.cpp
+++ tarea_2/A_candies.cpp
@@ -2,22 +2,15 @@
 #include <cmath>
 using namespace std;
 
-bool calculate_value(long long x, long long n, long long k){
-    long long value = x*((long long)pow(2,k)-1);
-    return n==value;
-}
+
 
 void calculate_x_values(long long n){
     long double n_log = ceil(log2(n+1));
-    for(long long x = 1; x<=n; x++){
-        bool encountered = false;
-        for(long long k=2; k<=n_log; k++){
-            encountered = calculate_value(x,n,k);
-            if(encountered){
-                cout << x << endl;
-            }
-        }
-        if(encountered){
+    for(long long k=2; k<=n_log; k++){
+        long long divisor = (long long)pow(2,k) - 1;
+        if(n%divisor == 0){
+            long long x = n / divisor;
+            cout<<x<<endl;
             break;
         }
     }
